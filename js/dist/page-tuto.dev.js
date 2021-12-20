80055611a18a91afaa6b364a5e14b77df7faafa0
Karim Badwork <karim@badwork.fr>
2021-12-20 21:27:52 +0100

    last corrections btn play tuto page

--- js/dist/page-tuto.dev.js
+++ js/dist/page-tuto.dev.js
@@ -4,13 +4,29 @@ if (document.querySelector('body.page-id-1301')) {
   var play = false;
   document.querySelector('#play').addEventListener('click', function () {
     if (play == false) {
-      document.querySelector('video').play();
+      document.querySelector('.d-sm-flex video').play();
       document.querySelector('#play').innerHTML = '<i class="fas fa-pause"></i>';
       play = true;
     } else {
-      document.querySelector('video').pause();
+      document.querySelector('.d-sm-flex video').pause();
       document.querySelector('#play').innerHTML = '<i class="fas fa-play"></i>';
       play = false;
+    }
+  });
+} // For HOMEPAGE 
+
+
+if (document.querySelector('body.home')) {
+  var _play = false;
+  document.querySelector('#play').addEventListener('click', function () {
+    if (_play == false) {
+      document.querySelector('#promenade .d-sm-flex video').play();
+      document.querySelector('#play').innerHTML = '<i class="fas fa-pause"></i>';
+      _play = true;
+    } else {
+      document.querySelector('#promenade .d-sm-flex video').pause();
+      document.querySelector('#play').innerHTML = '<i class="fas fa-play"></i>';
+      _play = false;
     }
   });
 }
--- js/page-tuto.js
+++ js/page-tuto.js
@@ -3,11 +3,29 @@ if (document.querySelector('body.page-id-1301')) {
 
     document.querySelector('#play').addEventListener('click', () => {
         if (play == false) {
-            document.querySelector('video').play()
+            document.querySelector('.d-sm-flex video').play()
             document.querySelector('#play').innerHTML = '<i class="fas fa-pause"></i>'
             play = true
         } else {
-            document.querySelector('video').pause()
+            document.querySelector('.d-sm-flex video').pause()
+            document.querySelector('#play').innerHTML = '<i class="fas fa-play"></i>'
+            play = false
+        }
+    })
+}
+
+// For HOMEPAGE 
+
+if (document.querySelector('body.home')) {
+    let play = false;
+
+    document.querySelector('#play').addEventListener('click', () => {
+        if (play == false) {
+            document.querySelector('#promenade .d-sm-flex video').play()
+            document.querySelector('#play').innerHTML = '<i class="fas fa-pause"></i>'
+            play = true
+        } else {
+            document.querySelector('#promenade .d-sm-flex video').pause()
             document.querySelector('#play').innerHTML = '<i class="fas fa-play"></i>'
             play = false
         }
